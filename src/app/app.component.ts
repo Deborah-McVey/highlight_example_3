60483a38304aca92d1eefc4ede18f1b442c01a5d
Deborah McVey <deborah.mcvey@gmail.com>
2024-05-11 22:21:36 -0400

    mouse over highlights text, changes backgroundColor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HighlightDirective } from './directives/highlight.directive';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HighlightDirective, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'highlight_example_3';
+  color = 'yellow';
 }
